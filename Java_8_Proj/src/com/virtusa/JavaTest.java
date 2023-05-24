1e85590349fe3d5696e18f435b8bd7288a9ed962
ADMIN <ADMIN@DESKTOP-BFTQ2K6>
2023-05-24 23:18:24 +0530

    Commit

---
+++ Java_8_Proj/src/com/virtusa/JavaTest.java
@@ -0,0 +1,12 @@
+package com.virtusa;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JavaTest.main()");
+		System.out.println("Welcom to java 8programming..!");
+
+	}
+
+}
